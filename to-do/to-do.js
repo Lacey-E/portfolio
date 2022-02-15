aeda111ad5416e6c2dcc51c9298a3d0cbb5c205b
lacey-e <buchiezim@gmail.com>
2022-02-15 21:56:27 +0100

    dc

--- to-do/to-do.js
+++ to-do/to-do.js
@@ -35,13 +35,7 @@ const listBtn = document.createElement('button');
     list.removeChild(listItem);
     }        
     
-//     listCheck.addEventListener
-// l
-    
-//     // ("a").click(function() {
-//     //     $(this).css("text-decoration", "line-through");
-//     //   });
-    
+
     
     listCheck.onclick = function(e) {
         listItem.style.textDecorationLine = "line-through"
@@ -56,40 +50,3 @@ const listBtn = document.createElement('button');
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const list = document.querySelector('ul');
-// const input = document.querySelector('input');
-// const button = document.querySelector('button');
-
-// button.onclick = function() {
-//   let myItem = input.value;
-//   input.value = '';
-
-//   const listItem = document.createElement('li');
-//   const listText = document.createElement('span');
-//   const listBtn = document.createElement('button');
-
-//   listItem.appendChild(listText);
-//   listText.textContent = myItem;
-//   listItem.appendChild(listBtn);
-//   listBtn.textContent = '❌';
-//   list.appendChild(listItem);
-
-//   listBtn.onclick = function(e) {
-//     list.removeChild(listItem);
-//   }
-
-//   input.focus();
-// }
