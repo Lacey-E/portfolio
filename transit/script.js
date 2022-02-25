9227d251f5e1e39e9283c75f702a75a253a6a0b8
lacey-e <buchiezim@gmail.com>
2022-02-25 15:54:20 +0100

    sdfsd

---
+++ transit/script.js
@@ -0,0 +1,28 @@
+window.addEventListener("load", drawImageToCanvas, false);
+
+    var canvas = document.getElementById("demo3");
+    var context = canvas.getContext("2d");
+    context.strokeStyle = "red";
+    var gradient = context.createLinearGradient(0, 0, 0, 200); 
+    var gradient = context.createLinearGradient(0, 0, 0, 200);
+    gradient.addColorStop(0, "blue"); 
+    gradient.addColorStop(1, "white"); 
+    context.fillStyle = gradient; 
+    context.fillRect(10, 10, 100, 100); 
+    context.strokeRect(10, 10, 100, 100); 
+
+
+function drawImageToCanvas() {
+    var canvas = document.getElementById("demo6");
+    var context = canvas.getContext("2d");
+    var image = document.getElementById("myImageElem");
+    context.drawImage(image, 0, 0); 
+}
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
+var image = document.getElementById("myImageElem");
+// draw the image at x=0 and y=0 on the canvas
+context.drawImage(image, 68, 68);
+var imageData = context.getImageData(0, 0, 1, 1);
+var pixelData = imageData.data;
+console.log(pixelData.length);
