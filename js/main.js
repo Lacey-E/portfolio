e0ea89844dd466713d0fd7c2829c8322b509088b
lacey-e <buchiezim@gmail.com>
2022-03-13 06:39:15 +0100

    jmd

--- js/main.js
+++ js/main.js
@@ -4,4 +4,12 @@ document.querySelector('#year').innerHTML = finalday
 
 
 document.querySelector('#time').innerHTML = date.toDateString();
-  
+const btn = document.getElementById('rainbow');
+
+const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
+
+function change() {      
+    document.body.style.background = rainbow[Math.floor(7*
+    Math.random())];
+}
+btn.addEventListener('click', change);
